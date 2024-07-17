f5eec3d8807ea8f05f71459bd249a5efd4d4ad2e
Daniel James Smith <djsmith85@users.noreply.github.com>
2024-07-17 22:10:10 +0200

    Fix service injection on DefaultSendFormService

--- libs/tools/send/send-ui/src/send-form/services/default-send-form.service.ts
+++ libs/tools/send/send-ui/src/send-form/services/default-send-form.service.ts
@@ -3,8 +3,7 @@ import { inject, Injectable } from "@angular/core";
 import { Send } from "@bitwarden/common/tools/send/models/domain/send";
 import { SendView } from "@bitwarden/common/tools/send/models/view/send.view";
 import { SendApiService } from "@bitwarden/common/tools/send/services/send-api.service.abstraction";
-import { SendService } from "@bitwarden/common/tools/send/services/send.service";
-import { InternalSendService } from "@bitwarden/common/tools/send/services/send.service.abstraction";
+import { SendService } from "@bitwarden/common/tools/send/services/send.service.abstraction";
 
 import { SendFormConfig } from "../abstractions/send-form-config.service";
 import { SendFormService } from "../abstractions/send-form.service";
@@ -12,7 +11,7 @@ import { SendFormService } from "../abstractions/send-form.service";
 @Injectable()
 export class DefaultSendFormService implements SendFormService {
   private sendApiService: SendApiService = inject(SendApiService);
-  private sendService: InternalSendService = inject(SendService);
+  private sendService = inject(SendService);
 
   async decryptSend(send: Send): Promise<SendView> {
     return await send.decrypt();
