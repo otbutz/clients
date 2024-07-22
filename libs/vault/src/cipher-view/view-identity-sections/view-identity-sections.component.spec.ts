0c4c6c4139f74890dfac0a6ed2954353fd2d9f5f
Nick Krantz <nick@livefront.com>
2024-07-22 15:46:21 -0500

    initial add of view identity sections

---
+++ libs/vault/src/cipher-view/view-identity-sections/view-identity-sections.component.spec.ts
@@ -0,0 +1,69 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
+import { IdentityView } from "@bitwarden/common/vault/models/view/identity.view";
+import { SectionHeaderComponent } from "@bitwarden/components";
+
+import { BitInputDirective } from "../../../../components/src/input/input.directive";
+
+import { ViewIdentitySectionsComponent } from "./view-identity-sections.component";
+
+describe("ViewIdentitySectionsComponent", () => {
+  let component: ViewIdentitySectionsComponent;
+  let fixture: ComponentFixture<ViewIdentitySectionsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ViewIdentitySectionsComponent],
+      providers: [{ provide: I18nService, useValue: { t: (key: string) => key } }],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewIdentitySectionsComponent);
+    component = fixture.componentInstance;
+    component.cipher = { identity: new IdentityView() } as CipherView;
+    fixture.detectChanges();
+  });
+
+  describe("personal details", () => {
+    it("dynamically shows the section", () => {
+      let personalDetailSection = fixture.debugElement.query(By.directive(SectionHeaderComponent));
+
+      expect(personalDetailSection).toBeNull();
+
+      component.cipher = {
+        identity: {
+          fullName: "Mr Ron Burgundy",
+        },
+      } as CipherView;
+
+      fixture.detectChanges();
+
+      personalDetailSection = fixture.debugElement.query(By.directive(SectionHeaderComponent));
+
+      expect(personalDetailSection).not.toBeNull();
+      expect(personalDetailSection.nativeElement.textContent).toBe("personalDetails");
+    });
+
+    it("populates personal detail fields", () => {
+      component.cipher = {
+        identity: {
+          fullName: "Mr Ron Burgundy",
+          company: "Channel 4 News",
+          username: "ron.burgundy",
+        },
+      } as CipherView;
+
+      fixture.detectChanges();
+
+      const fields = fixture.debugElement.queryAll(By.directive(BitInputDirective));
+
+      expect(fields[0].nativeElement.value).toBe("Mr Ron Burgundy");
+      expect(fields[1].nativeElement.value).toBe("ron.burgundy");
+      expect(fields[2].nativeElement.value).toBe("Channel 4 News");
+    });
+  });
+});
---
+++ libs/vault/src/cipher-view/view-identity-sections/view-identity-sections.component.ts
@@ -0,0 +1,38 @@
+import { NgIf } from "@angular/common";
+import { Component, Input } from "@angular/core";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
+import {
+  CardComponent,
+  FormFieldModule,
+  SectionComponent,
+  SectionHeaderComponent,
+  TypographyModule,
+} from "@bitwarden/components";
+
+@Component({
+  standalone: true,
+  selector: "app-view-identity-sections",
+  templateUrl: "./view-identity-sections.component.html",
+  imports: [
+    NgIf,
+    JslibModule,
+    CardComponent,
+    SectionComponent,
+    SectionHeaderComponent,
+    TypographyModule,
+    FormFieldModule,
+  ],
+})
+export class ViewIdentitySectionsComponent {
+  @Input() cipher: CipherView;
+
+  constructor() {}
+
+  /** Returns true when any of the "personal detail" attributes are populated */
+  hasPersonalDetails(): boolean {
+    const { username, company, fullName } = this.cipher.identity;
+    return Boolean(fullName || username || company);
+  }
+}
