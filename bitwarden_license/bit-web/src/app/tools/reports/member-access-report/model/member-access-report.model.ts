2a3f590e9acb69183714796ba268d9efa01b17bb
Tom <ttalty@bitwarden.com>
2024-08-15 14:23:33 -0400

    Comments on the model

--- bitwarden_license/bit-web/src/app/tools/reports/member-access-report/model/member-access-report.model.ts
+++ bitwarden_license/bit-web/src/app/tools/reports/member-access-report/model/member-access-report.model.ts
@@ -1,9 +1,13 @@
 import { EncString } from "@bitwarden/common/platform/models/domain/enc-string";
 
+/**
+ * Details for the parents MemberAccessReport
+ */
 export type MemberAccessDetails = {
   collectionId: string;
   groupId: string;
   groupName: string;
+  // Comes encrypted from the server
   collectionName: EncString;
   itemCount: number;
   readOnly: boolean;
